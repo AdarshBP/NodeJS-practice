ccdf9135ca071d0cdd7eb2f45fde7260921a3bf6
AdarshBP <bpadarsh8@gmail.com>
2019-12-18 10:16:17 +0530

    added 404 page not found error page for invalid URL

--- shopping site/app.js
+++ shopping site/app.js
@@ -7,13 +7,17 @@ const shopRoutes=require('./routes/shop');
 
 //initializing
 const app=express();
-app.listen(3000);
 app.use(bodyParser.urlencoded({extend:false}));
 
 //routing 
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use((req,res,next) => {
+    res.status(404).send('<h1> oops!! page not found , try typing the proper URL </h1>');
+});
 
 
+//listing to port 3000
+app.listen(3000);
 
 
