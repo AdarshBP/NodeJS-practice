c8c7ccfea4ca1f39374a34394e47b7d401cbd682
AdarshBP <bpadarsh8@gmail.com>
2019-12-22 17:38:17 +0530

    tried git GUI no additional changes

--- shopping site/app.js
+++ shopping site/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
